e2f3ea5d0a5cca12c4025b402f89fac01fb7694d
Marcell Endrey <marcell.endrey@prezi.com>
2018-10-14 21:44:31 +0200

    install express for later integration tests

---
+++ test/jiraMock.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({"foo": "bar"}))
+});
+
+const server = app.listen(8081)
