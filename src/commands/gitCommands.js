9ceb82dfd575167d003f09abfdc49fbb3d5c0b36
Marcell Endrey <marcell.endrey@prezi.com>
2018-10-29 22:21:25 +0100

    flatten the pr command a bit

--- src/commands/gitCommands.js
+++ src/commands/gitCommands.js
@@ -54,63 +54,69 @@ const checkCommand = () => {
 };
 
 const prCommand = () => {
-    const {GITHUB_USERNAME, GITHUB_API_TOKEN} = process.env;
-    if (!GITHUB_USERNAME || !GITHUB_API_TOKEN) {
-        throw Error("missing github credentials");
-    }
     exec("git ls-remote --get-url origin", (error, stdout) => {
-        const [owner, repo] = getRepoCoordinates(stdout);
+        const {owner, repo} = getRepoCoordinates(stdout);
         exec("git rev-parse --abbrev-ref HEAD", (error, stdout) => {
             const branchName = stdout.trim();
-            exec(`git push --set-upstream origin ${branchName}`, () => {
-                const editor = readFromConfig("editor");
-                const filename = path.join(
-                    JIRAF_HOME_FOLDER,
-                    `pullrequest.${crypto.randomBytes(20).toString("hex")}.md`
-                );
-                const preparedTemplate = interpolate(PULL_REQUEST_TEMPLATE, {
-                    key: readActiveCardKey(),
-                    jiraUrlBase: readFromConfig("jiraUrlBase"),
-                });
-                fs.writeFileSync(filename, preparedTemplate);
-                const childProcess = exec(`${editor} ${filename}`);
-                childProcess.on("close", () => {
-                    const description = fs.readFileSync(filename).toString();
-                    const descriptionLines = description.split(/\r?\n/);
-                    const title = descriptionLines[0];
-                    const body = descriptionLines.slice(1, descriptionLines.length - 1).join("\n");
-                    axios({
-                        method: "post",
-                        url: `https://api.github.com/repos/${owner}/${repo}/pulls`,
-                        auth: {
-                            username: GITHUB_USERNAME,
-                            password: GITHUB_API_TOKEN,
-                        },
-                        data: {
-                            title: title,
-                            head: branchName,
-                            base: "master",
-                            body: body,
-                        },
-                    })
-                        .then(response => {
-                            fs.unlink(filename, () => {});
-                            console.log(response.data.html_url);
-                        })
-                        .catch(error => console.log(error.response));
-                });
-            });
+            exec(
+                `git push --set-upstream origin ${branchName}`,
+                editDescriptionAndCreatePullRequest({owner, repo, branchName})
+            );
         });
     });
 };
 
+const editDescriptionAndCreatePullRequest = ({owner, repo, branchName}) => {
+    const editor = readFromConfig("editor");
+    const descriptionFileName = path.join(
+        JIRAF_HOME_FOLDER,
+        `pullrequest.${crypto.randomBytes(20).toString("hex")}.md`
+    );
+    const preparedTemplate = interpolate(PULL_REQUEST_TEMPLATE, {
+        key: readActiveCardKey(),
+        jiraUrlBase: readFromConfig("jiraUrlBase"),
+    });
+    fs.writeFileSync(descriptionFileName, preparedTemplate);
+    const childProcess = exec(`${editor} ${descriptionFileName}`);
+    childProcess.on("close", () => createPullRequest({owner, repo, branchName, descriptionFileName}));
+};
+
+const createPullRequest = ({owner, repo, branchName, descriptionFileName}) => {
+    const {GITHUB_USERNAME, GITHUB_API_TOKEN} = process.env;
+    if (!GITHUB_USERNAME || !GITHUB_API_TOKEN) {
+        throw Error("missing github credentials");
+    }
+    const description = fs.readFileSync(descriptionFileName).toString();
+    const descriptionLines = description.split(/\r?\n/);
+    const title = descriptionLines[0];
+    const body = descriptionLines.slice(1, descriptionLines.length - 1).join("\n");
+    axios({
+        method: "post",
+        url: `https://api.github.com/repos/${owner}/${repo}/pulls`,
+        auth: {
+            username: GITHUB_USERNAME,
+            password: GITHUB_API_TOKEN,
+        },
+        data: {
+            title: title,
+            head: branchName,
+            base: "master",
+            body: body,
+        },
+    })
+        .then(response => console.log(response.data.html_url))
+        .catch(error => console.log(error.response))
+        .finally(() => fs.unlink(descriptionFileName, () => {}));
+};
+
 const getRepoCoordinates = remote => {
-    return remote
+    const [owner, repo] = remote
         .trim()
         .replace("https://github.com/", "")
         .replace("git@github.com:", "")
         .replace(".git", "")
         .split("/");
+    return {owner, repo};
 };
 
 module.exports = {
