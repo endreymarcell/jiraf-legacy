24a669fa9363c2c237197e3da6993dbaf198dafd
Marcell Endrey <marcell.endrey@prezi.com>
2018-10-31 17:00:30 +0100

    let's also mount package.json to get script changes instantly

--- scripts/run_tests_locally.sh
+++ scripts/run_tests_locally.sh
@@ -2,6 +2,7 @@
 docker run -t \
     -v "$(pwd)/src:/jiraf/src" \
     -v "$(pwd)/test:/jiraf/test" \
+    -v "$(pwd)/package.json:/jiraf/package.json" \
     -e "TERM=xterm-256color" \
     endreymarca/jiraf-testing \
     npm test
