36a6f45c2e8454fbc0545bea79516abaac46a64d
Marcell Endrey <marcell.endrey@prezi.com>
2018-11-01 19:24:28 +0100

    fix setup

--- src/setup.js
+++ src/setup.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const {JIRAF_CONFIG_FILE} = require("./const");
 
-const defaultConfig = JSON.dumps({
+const defaultConfig = {
     jiraUrlBase: "https://JIRAF.atlassian.net",
     editor: "vim",
     statusTemplate: "{{key}} [{{status}}] {{summary}} ({{assignee}})",
@@ -10,6 +10,6 @@ const defaultConfig = JSON.dumps({
         review: ["move codereview", "pr"],
         qa: ["move validation", "assign QA.PERSON"],
     },
-});
+};
 
-fs.writeFileSync(JIRAF_CONFIG_FILE, defaultConfig);
+fs.writeFileSync(JIRAF_CONFIG_FILE, JSON.stringify(defaultConfig, null, "   "));
