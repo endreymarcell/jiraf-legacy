fa16b41cfd171902e9e816b18554a4ec5e49fadc
Marcell Endrey <marcell.endrey@prezi.com>
2018-11-06 22:40:57 +0100

    simplify test scripts a bit. could be even clearer.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-docker run -t \
+if [ -f /.dockerenv ]; then
+    npm run test:docker "$1"
+else
+    docker run -t \
     -v "$(pwd)/src:/jiraf/src" \
     -v "$(pwd)/test:/jiraf/test" \
     -v "$(pwd)/package.json:/jiraf/package.json" \
     -e "TERM=xterm-256color" \
     endreymarca/jiraf-testing \
-    npm run test:docker:grep "${1:-}"
+    npm run test:docker "$1"
+fi
