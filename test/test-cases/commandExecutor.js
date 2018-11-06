c4a7c99b8c2ff16126b9e12d08954871c6b57716
Marcell Endrey <marcell.endrey@prezi.com>
2018-11-06 22:50:30 +0100

    add some command execution tests

--- test/test-cases/commandExecutor.js
+++ test/test-cases/commandExecutor.js
@@ -1,4 +1,11 @@
+const fs = require("fs");
+const {expect} = require("chai");
+
 const {clearBeforeTests} = require("./utils/utils");
+const {expectError, expectOutput, expectInSession} = require("./utils/shorthands");
+const {errorMessages} = require("../../src/utils/messages");
+const {updateInConfig} = require("../../src/utils/storageHandler");
+const {JIRAF_SESSION_FILE} = require("../../src/const");
 
 describe("commandExecutor", () => {
     beforeEach(() => {
@@ -9,19 +16,34 @@ describe("commandExecutor", () => {
         done();
     });
 
-    it.skip("should fail for an unknown command", done => {
-        done();
+    it("should fail for an unknown command", done => {
+        expectError("jiraf jiraf", errorMessages.unknownCommand("jiraf"), done);
     });
 
-    it.skip("should run a command specified by a shortcut", done => {
-        done();
+    it("should run a command specified by a shortcut", done => {
+        updateInConfig("shortcuts", {test: ["debug"]});
+        expectOutput("jiraf test", "jiraf is responsive", done);
     });
 
-    it.skip("should run multiple command specified by a shortcut", done => {
-        done();
+    it("should run multiple command specified by a shortcut", done => {
+        updateInConfig("shortcuts", {test: ["debug", "set GRZ-1"]});
+        expectOutput("jiraf test", "jiraf is responsive", () => {
+            const session = JSON.parse(fs.readFileSync(JIRAF_SESSION_FILE));
+            expect(session.activeCardDetails).to.deep.eq({
+                key: "GRZ-1",
+                summary: "The future is coming on",
+                description: "I ain't happy, I'm feeling glad, I got sunshine in a bag",
+                status: "To Do",
+                assignee: "clint.eastwood",
+                priority: "Prio3 - Medium",
+                estimate: 3,
+            });
+            done();
+        });
     });
 
-    it.skip("should substitute the argument when a shortcut specifies that", done => {
-        done();
+    it("should substitute the argument when a shortcut specifies that", done => {
+        updateInConfig("shortcuts", {test: ["set {}"]});
+        expectInSession("jiraf test GRZ-4", {key: "activeCardKey", value: "GRZ-4"}, done);
     });
 });
