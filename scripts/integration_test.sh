0c106f548264e7aa1f66b35d694a9a2f59aaa16d
Marcell Endrey <marcell.endrey@prezi.com>
2018-11-14 16:50:10 +0100

    automatically remove test containers

--- scripts/integration_test.sh
+++ scripts/integration_test.sh
@@ -2,7 +2,7 @@
 if [ -f /.dockerenv ]; then
     npm run integration:docker
 else
-    docker run -t \
+    docker run --rm -t \
     -v "$(pwd)/src:/jiraf/src" \
     -v "$(pwd)/test:/jiraf/test" \
     -v "$(pwd)/package.json:/jiraf/package.json" \
